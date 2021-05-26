94ebc144a52ef4ecaf7c1f5ffbba131a78a27f0d
g <umacgamze49@gmail.com>
2021-05-26 15:47:45 +0200

    promise.js

--- app.js
+++ app.js
@@ -15,11 +15,29 @@ function loginUser(email, passsword, callback) {
    setTimeout(()=> {
        console.log("Now we have the data");
        callback({userEmail: email});
-   }, 5000) 
+   }, 3000) 
 }
+
+function getuserVideos(email, callback) {
+ setTimeout(() => {
+     callback(["video1", "video2", "video3"])
+ }, 2000)    
+}
+
+function videoDetails(video, callback) {
+    setTimeout(() => {
+        callback("title of the video")
+    }, 2000);
+   }
 
 const user = loginUser("gamze@goomail.com", 123456, user => {
     console.log(user);
+    getuserVideos(user.userEmail, videos => {
+        console.log(videos);
+        videoDetails(videos[0], title =>{
+            console.log(title);
+        });
+    });
 });
 
 
